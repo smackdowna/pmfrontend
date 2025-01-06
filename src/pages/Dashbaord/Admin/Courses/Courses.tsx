fba559958717f1180a7a56631a08bb316dc08fac
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-06 09:54:32 +0545

    ui/passing-widths-dashCards

--- src/pages/Dashbaord/Admin/Courses/Courses.tsx
+++ src/pages/Dashbaord/Admin/Courses/Courses.tsx
@@ -1,22 +1,27 @@
 import DashboardCard from "../../../../components/Reusable/DashboardCard/DashboardCard";
 import DashboardHeader from "../../../../components/Reusable/DashboardHeader/DashboardHeader";
 const AdminCourses = () => {
-    return (
-        <>
-            <div className="flex items-center justify-between w-full">
-                <DashboardHeader pageName="My Courses" pageDesc="All your courses in one place." />
-                <button className="px-[14px] py-4 bg-primary-10 text-white text-base font-medium leading-5 tracking-tighter rounded-[10px]">Add a course</button>
-            </div>
-            <div className="flex items-center justify-start w-full flex-wrap gap-4">
-                <DashboardCard title="Total Courses" count={5} />
-                <DashboardCard title="Total Courses" count={5} />
-                <DashboardCard title="Total Courses" count={5} />
-                <DashboardCard title="Total Courses" count={5} />
-                <DashboardCard title="Total Courses" count={5} />
-                <DashboardCard title="Total Courses" count={5} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center justify-between w-full">
+        <DashboardHeader
+          pageName="My Courses"
+          pageDesc="All your courses in one place."
+        />
+        <button className="px-[14px] py-4 bg-primary-10 text-white text-base font-medium leading-5 tracking-tighter rounded-[10px]">
+          Add a course
+        </button>
+      </div>
+      <div className="flex items-center justify-start w-full flex-wrap gap-4">
+        <DashboardCard title="Total Courses" count={5} />
+        <DashboardCard title="Total Courses" count={5} />
+        <DashboardCard title="Total Courses" count={5} />
+        <DashboardCard title="Total Courses" count={5} />
+        <DashboardCard title="Total Courses" count={5} />
+        <DashboardCard title="Total Courses" count={5} />
+      </div>
+    </>
+  );
 };
 
 export default AdminCourses;
