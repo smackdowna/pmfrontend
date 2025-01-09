bddcc8b9a62d27519cf2f25aecbe429b96487094
adhikareeprayush <adhikareeprayush@gmail.com>
2025-01-09 13:59:19 +0545

    dashboard layout changes

--- src/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -3,7 +3,9 @@ import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <div className="flex w-full">
-      <div className="bg-primary-10 w-44 h-screen p-10 text-white">Sidebar</div>
+      <div className="bg-primary-10 w-44 h-screen p-10 text-white">
+        This is the sidebar
+      </div>
       <div className="flex flex-col gap-10 w-full p-6 bg-neutral-80">
         <div className="p-6 flex flex-col w-full gap-6 ">
           <Outlet />
