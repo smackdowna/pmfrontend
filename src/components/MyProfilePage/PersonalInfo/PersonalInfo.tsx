485877aac1429357a0f423e9861a687886f7f23f
Rahul Sutradhar <rahulsd380@gmail.com>
2025-01-27 21:06:50 +0600

    Added all indian states

--- src/components/MyProfilePage/PersonalInfo/PersonalInfo.tsx
+++ src/components/MyProfilePage/PersonalInfo/PersonalInfo.tsx
@@ -10,7 +10,36 @@ type TPersonalInfo = {
 
 const PersonalInfo: React.FC<TPersonalInfo> = ({ register, errors, mobileNumber }) => {
   const genderOptions = ["Male", "Female", "Other"];
-  const stateOptions = ["Maharashtra", "Gujrat"];
+  const stateOptions = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+  ];
   const countryOptions = ["India"];
   return (
     <div className="flex flex-col gap-4">
