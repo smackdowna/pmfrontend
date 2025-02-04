295652218fff44ddc6f02698d5e92dbc2bff6ce8
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-04 20:41:47 +0600

    Changed featured to upcoming course

--- src/components/HomePage/FeaturedOn/FeaturedOn.tsx
+++ src/components/HomePage/FeaturedOn/FeaturedOn.tsx
@@ -2,15 +2,38 @@ import Marquee from "react-fast-marquee";
 import { IMAGES } from "../../../assets";
 
 const FeaturedOn = () => {
-    const logos = [IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo,]
+    const logos = [IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo, IMAGES.pmGurukulLogo];
+
+    const title = "Learn Modern Web Development with JavaScript";
     return (
         <div className="bg-[#fff6eb] py-[64px] flex flex-col gap-[38px]">
-            <h1 className="text-primary-10 text-[36px] md:text-[40px] font-bold leading-[40px] xl:leading-[48px] text-center">As Featured On</h1>
+            <h1 className="text-primary-10 text-[36px] md:text-[40px] font-bold leading-[40px] xl:leading-[48px] text-center">Our Upcoming Courses </h1>
 
             <Marquee speed={50}>
-                {logos.map((logo, index) => (
-                    <div className="w-[257px] h-[133px] flex justify-center items-center bg-white rounded-[24px] mr-[22px]">
-                        <img key={index} src={logo} alt={''} className="" />
+                {logos.map((_, index) => (
+                    <div key={index} className="bg-white border border-neutral-45 rounded-[24px] font-Inter w-[312px] h-[310px] mr-10">
+                        <img src={IMAGES.courseImg} alt="" className="rounded-t-[24px] w-full h-[200px] object-cover" />
+
+                        <div className="p-5 flex flex-col gap-4">
+
+                            <h1 className="text-primary-10 text-xl leading-7 font-semibold capitalize">
+                                {title.length > 20 ? `${title.slice(0, 20)}...` : title}
+                            </h1>
+
+
+                            <div className="flex items-center gap-2">
+                                {/* <img src={ICONS.avatar} alt={""} className="size-8 rounded-full" /> */}
+                                <h1 className="text-primary-10/70 text-sm leading-6">By -</h1>
+                                <h1 className="text-primary-10 font-medium leading-6">PMGURUKUL</h1>
+                            </div>
+
+
+                            {/* <Ripple styles="rounded-lg">
+                                <Link to={`/course/${_id}`} className="bg-neutral-60 border border-neutral-55 py-[10px] px-4 text-primary-10 text-sm leading-5 font-semibold w-full rounded-lg text-center">
+                                    View Course
+                                </Link>
+                            </Ripple> */}
+                        </div>
                     </div>
                 ))}
             </Marquee>
