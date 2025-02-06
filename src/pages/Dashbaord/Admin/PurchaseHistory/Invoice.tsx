0a581895d57ff6b32ba1b7ed122c1b43b3b87f4b
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-06 11:41:06 +0600

    Changed customer support bg

--- src/pages/Dashbaord/Admin/PurchaseHistory/Invoice.tsx
+++ src/pages/Dashbaord/Admin/PurchaseHistory/Invoice.tsx
@@ -7,20 +7,28 @@ import { formatDate } from '../../../../utils/formatDate';
 const styles = StyleSheet.create({
   page: { padding: 30, fontSize: 12, position: 'relative' },
   headerContainer: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 },
-  logo: { width: 200, height: 70 },
-  address: { textAlign: 'right' },
+  logo: { width: 180, height: 60 },
+  address: { textAlign: 'right', fontSize: 10 },
   section: { marginBottom: 10, backgroundColor: '#F2F2F2', padding: 8, borderRadius: 6 },
   section2: { marginBottom: 10 },
   boldText: { fontWeight: 'bold', color: 'black' }, // Black bold titles
   normalText: { color: '#4F4F4F' }, // Gray normal text
   table: { width: '100%', marginTop: 10, borderWidth: 1, borderColor: '#ccc' },
   tableRow: {
+    borderRight: 2,
     flexDirection: 'row',
     borderBottomWidth: 1,
     borderColor: '#ccc',
     alignItems: 'center',
   },
+  rowFontSize: {
+    fontSize: 10
+  },
   tableCell: { flex: 1, padding: 5, textAlign: 'center' },
+  cellWithBorder: {
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
   tableHeader: { backgroundColor: '#f2f2f2', fontWeight: 'bold' },
   declarationRow: {
     borderTopWidth: 1,
@@ -104,45 +112,48 @@ const Invoice = ({ order }: { order: TOrders }) => {
         </View>
 
         {/* Course Table */}
+        {/* Course Table */}
         <View style={styles.table}>
           {/* Table Header */}
           <View style={[styles.tableRow, styles.tableHeader]}>
-            <Text style={styles.tableCell}>Course Name</Text>
-            <Text style={styles.tableCell}>Description</Text>
-            <Text style={styles.tableCell}>Qty</Text>
-            <Text style={styles.tableCell}>Price</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>Description</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>Qty</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>HSN/SAC</Text>
+            <Text style={styles.tableCell}>Amount</Text>
           </View>
 
-          {/* Table Content */}
-          {order?.course?.map((item: any, index: number) => (
-            <View key={index} style={styles.tableRow}>
-              <Text style={styles.tableCell}>{item?.title}</Text>
-              <Text style={styles.tableCell}>{item?.description}</Text>
-              <Text style={styles.tableCell}>{order?.course?.length || 0}</Text>
-              <Text style={styles.tableCell}>{order?.discountedPrice}</Text>
-            </View>
-          ))}
-
-          {/* GST Row */}
-          <View style={styles.tableRow}>
-            <Text style={styles.tableCell}>
-              GST ({order?.gst}%)
-            </Text>
-            <Text style={styles.tableCell}></Text>
-            <Text style={styles.tableCell}></Text>
-            <Text style={styles.tableCell}>
-              {(order?.discountedPrice * order?.gst) / 100}
-            </Text>
+          {/* Course Details */}
+          <View style={[styles.tableRow, styles.rowFontSize]}>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>Marketing Mastery</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>1</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>999293</Text>
+            <Text style={styles.tableCell}>1499</Text>
           </View>
 
-          {/* Total Row */}
+
+          {/* Subtotal */}
+          <View style={[styles.tableRow, styles.rowFontSize]}>
+            <Text style={styles.tableCell}></Text>
+            <Text style={styles.tableCell}></Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder, { textAlign: 'right', fontWeight: 'bold' }]}>Sub Total:</Text>
+            <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>1270.34</Text>
+          </View>
+
+          {/* Tax Row */}
+          <View style={[styles.tableRow, styles.rowFontSize]}>
+            <Text style={[styles.tableCell, styles.cellWithBorder, { fontWeight: 'bold' }]}>Tax 19%:</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>CGST: 9%</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder]}>CGST Amt: 114.33</Text>
+            <Text style={[styles.tableCell, styles.cellWithBorder, { fontWeight: 'bold' }]}>12312</Text>
+          </View>
+
+
+          {/* Total */}
           <View style={styles.tableRow}>
-            <Text style={styles.tableCell}>Total</Text>
+            <Text style={[styles.tableCell, { textAlign: 'right' }]}></Text>
             <Text style={styles.tableCell}></Text>
-            <Text style={styles.tableCell}></Text>
-            <Text style={styles.tableCell}>
-              {order?.totalPrice?.toFixed(2)}
-            </Text>
+            <Text style={[styles.tableCell, { textAlign: 'right', fontWeight: 'bold' }]}>Total:</Text>
+            <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>1499</Text>
           </View>
         </View>
 
@@ -150,8 +161,10 @@ const Invoice = ({ order }: { order: TOrders }) => {
         <View style={styles.table}>
           <Text style={styles.declarationRow}>
             Declaration: We declare that this invoice shows the actual price of the goods/services
-            described and that all particulars are true and correct. This is a Computer Generated
-            Invoice. No Signature Required.
+            described and that all particulars are true and correct.
+          </Text>
+          <Text style={[styles.declarationRow, { fontWeight: 'bold' }]}>
+            This is a Computer Generated Invoice No Signature Required.
           </Text>
         </View>
 
